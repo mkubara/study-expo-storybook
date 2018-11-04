c913619b0da1615f3d5b9fe68a69e99badddf34b
mkubara <masahiko_kubara@relationsgroup.co.jp>
2018-11-04 19:59:03 +0900

    Fix path

--- src/client/native/components/pages/App/index.js
+++ src/client/native/components/pages/App/index.js
@@ -16,8 +16,8 @@ export default class App extends React.Component {
 
   async loadFonts() {
     await Expo.Font.loadAsync({
-      Roboto: require('./node_modules/native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('./node_modules/native-base/Fonts/Roboto_medium.ttf'),
+      Roboto: require('../../../../../../node_modules/native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('../../../../../../node_modules/native-base/Fonts/Roboto_medium.ttf'),
     });
     this.setState({ isReady: true });
   }
