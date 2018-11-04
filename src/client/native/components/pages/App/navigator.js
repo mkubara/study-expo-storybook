9526e7548632ca6ca0c8754bec58271e313f20cc
mkubara <masahiko_kubara@relationsgroup.co.jp>
2018-11-04 19:59:32 +0900

    Fix navigator exporting method

--- src/client/native/components/pages/App/navigator.js
+++ src/client/native/components/pages/App/navigator.js
@@ -5,15 +5,15 @@ import { Icon } from 'native-base';
 import Hello from '../Hello';
 import Another from '../Another';
 
-export default function AppNavigator() {
-  return createDrawerNavigator({
-    Hello: {
-      screen: Hello,
-      navigationOptions: { drawerLabel: 'Hello', drawerIcon: () => <Icon name="home" /> },
-    },
-    Another: {
-      screen: Another,
-      navigationOptions: { drawerLabel: 'Another', drawerIcon: () => <Icon name="person" /> },
-    },
-  });
-}
+const AppNavigator = createDrawerNavigator({
+  Hello: {
+    screen: Hello,
+    navigationOptions: { drawerLabel: 'Hello', drawerIcon: () => <Icon name="home" /> },
+  },
+  Another: {
+    screen: Another,
+    navigationOptions: { drawerLabel: 'Another', drawerIcon: () => <Icon name="person" /> },
+  },
+});
+
+export default AppNavigator;
